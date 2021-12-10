ea1cad1d02fa032972b82b87a49be389f2756656
oPensyLar <vmgomez.dev@protonmail.com>
2021-12-10 12:04:05 -0400

    add multiple patterns

--- main.py
+++ main.py
@@ -4,7 +4,7 @@ import pyad.adquery
 q = pyad.adquery.ADQuery()
 q.execute_query(attributes=["distinguishedName", "description"], where_clause="objectClass = 'computer'")
 headers = ["host_found"]
-pattern_hostname = "el"
+pattern_hostname = ["el", "los", "audi"]
 hosts_found = []
 
 for row in q.get_results():
@@ -12,9 +12,10 @@ for row in q.get_results():
     str_split = str_split[0].split("=")
     hostname = str_split[1].lower()
 
-    if hostname.find(pattern_hostname) is not -1:
-        print("Found " + hostname)
-        hosts_found.append([hostname])
+    for c_patt in pattern_hostname:
+    	if hostname.find(c_patt) is not -1:
+        	print("Found " + hostname)
+        	hosts_found.append([hostname])
 
 csv = csv_writer.CsvWriter("output.csv", headers, hosts_found)
 csv.create_csv()
